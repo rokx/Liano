f50908f61dbfba969ff67eca425c181347e2b1d1
rokxer <rokxer@gmail.com>
2025-09-11 23:58:15 +0200

    first commit of app

---
+++ app/src/main/java/com/example/liano/MainActivity.kt
@@ -0,0 +1,121 @@
+package com.example.liano
+
+import android.Manifest
+import android.content.pm.PackageManager
+import android.os.Bundle
+import android.util.Log
+import android.widget.TextView
+import androidx.appcompat.app.AppCompatActivity
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.core.app.ActivityCompat
+import androidx.core.content.ContextCompat
+import be.tarsos.dsp.AudioDispatcher
+import be.tarsos.dsp.io.android.AudioDispatcherFactory
+import be.tarsos.dsp.pitch.PitchProcessor
+import com.example.liano.ui.theme.LianoTheme
+import kotlin.math.log2
+import kotlin.math.roundToInt
+
+class MainActivity : AppCompatActivity() {
+
+    private val RECORD_AUDIO_PERMISSION = Manifest.permission.RECORD_AUDIO
+    private val REQUEST_MIC = 100
+    private var dispatcher: AudioDispatcher? = null
+
+    private lateinit var pitchText: TextView
+
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        pitchText = findViewById(R.id.pitchText)
+
+        requestMicPermission()
+    }
+
+    private fun requestMicPermission() {
+        if (ContextCompat.checkSelfPermission(this, RECORD_AUDIO_PERMISSION)
+            != PackageManager.PERMISSION_GRANTED
+        ) {
+            ActivityCompat.requestPermissions(
+                this,
+                arrayOf(RECORD_AUDIO_PERMISSION),
+                REQUEST_MIC
+            )
+        } else {
+            startPitchDetection()
+        }
+    }
+
+    override fun onRequestPermissionsResult(
+        requestCode: Int,
+        permissions: Array<out String>,
+        grantResults: IntArray
+    ) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults)
+        if (requestCode == REQUEST_MIC &&
+            grantResults.isNotEmpty() &&
+            grantResults[0] == PackageManager.PERMISSION_GRANTED
+        ) {
+            startPitchDetection()
+        }
+    }
+
+    private fun startPitchDetection() {
+        dispatcher = AudioDispatcherFactory.fromDefaultMicrophone(22050, 1024, 0)
+
+        val pitchDetector = PitchProcessor(
+            PitchProcessor.PitchEstimationAlgorithm.YIN,
+            22050f,
+            1024
+        ) { pitchDetectionResult, _ ->
+            val pitchInHz = pitchDetectionResult.pitch
+            runOnUiThread {
+                if (pitchInHz > 0) {
+                    Log.d("Pitch", "Detected pitch: $pitchInHz Hz")
+                    val noteName = frequencyToNoteName(pitchInHz)
+                    pitchText.text = "$noteName ($pitchInHz Hz)"
+
+                }
+            }
+        }
+
+        dispatcher?.addAudioProcessor(pitchDetector)
+
+        Thread {
+            dispatcher?.run()
+        }.start()
+    }
+
+    private fun frequencyToNoteName(freq: Float): String {
+        val noteNames = arrayOf(
+            "C", "C#", "D", "D#", "E", "F", "F#", "G", "G#", "A", "A#", "B"
+        )
+        val A4 = 440.0
+        val noteNumber = (12 * (log2(freq / A4)) + 69).roundToInt()
+        val name = noteNames[noteNumber % 12]
+        val octave = noteNumber / 12 - 1
+        return "$name$octave"
+    }
+
+}
+
+@Composable
+fun Greeting(name: String, modifier: Modifier = Modifier) {
+    Text(
+        text = "Hello $name!",
+        modifier = modifier
+    )
+}
+
+@Preview(showBackground = true)
+@Composable
+fun GreetingPreview() {
+    LianoTheme {
+        Greeting("Android")
+    }
+}
