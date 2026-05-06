112b082792970dad1a0757145bf6f8fc274ac94d
rokxer <rokxer@gmail.com>
2026-05-06 22:37:01 +0200

    fix(ui): remove stale compose preview

--- app/src/main/java/com/example/liano/MainActivity.kt
+++ app/src/main/java/com/example/liano/MainActivity.kt
@@ -33,7 +33,6 @@ class MainActivity : AppCompatActivity() {
     private var dispatcher: AudioDispatcher? = null
 
     private lateinit var pitchText: TextView
-    private var pitchLevelView: com.example.liano.ui.PitchLevelView? = null
 
     private lateinit var greatWorkText: TextView
     private lateinit var taskText: TextView
@@ -47,8 +46,6 @@ class MainActivity : AppCompatActivity() {
         setContentView(R.layout.activity_main)
 
         pitchText = findViewById(R.id.pitchText)
-        pitchLevelView = findViewById(R.id.pitchLevelView)
-        pitchLevelView?.setRange(80f, 300f)
 
         greatWorkText = findViewById(R.id.greatWorkText)
         taskText = findViewById(R.id.taskText)
@@ -215,21 +212,5 @@ class MainActivity : AppCompatActivity() {
         val octave = noteNumber / 12 - 1
 
         return "$name$octave"
-    }
-}
-
-@Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-}
-
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    LianoTheme {
-        Greeting("Android")
     }
 }
